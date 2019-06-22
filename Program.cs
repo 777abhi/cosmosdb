b1e8947ba596047956b166941a373b563a8884b2
abhinav <777abhi@gmail.com>
2019-06-23 02:19:40 +0530

    consolidate similar CSVs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
-using Microsoft.Azure.Documents;
-using Microsoft.Azure.Documents.Client;
-using Newtonsoft.Json;
+using System.Text;
+
 
 namespace testconsoleappcosmosdb
 {
@@ -13,6 +12,46 @@ namespace testconsoleappcosmosdb
     public class Program
     {
 
+
+        public static void Main(string[] args) {
+
+            Program p = new Program();
+            p.Test();
+
+        }
+
+
+        public void Test()
+        {
+
+            string sourceFolder = @"D:\myFiles\c\csvfiles";
+            string destinationFile = @"D:\myFiles\c\csvfiles\CSV_Files_Combined.csv";
+
+            // Specify wildcard search to match CSV files that will be combined
+            string[] filePaths = Directory.GetFiles(sourceFolder, "CSV_File_Number?.csv");
+            StreamWriter fileDest = new StreamWriter(destinationFile, true);
+
+            int i;
+            for (i = 0; i < filePaths.Length; i++)
+            {
+                string file = filePaths[i];
+
+                string[] lines = File.ReadAllLines(file);
+
+                if (i > 0)
+                {
+                    lines = lines.Skip(1).ToArray(); // Skip header row for all but first file
+                }
+
+                foreach (string line in lines)
+                {
+                    fileDest.WriteLine(line);
+                }
+            }
+
+            fileDest.Close();
+
+        }
 
     }
         
